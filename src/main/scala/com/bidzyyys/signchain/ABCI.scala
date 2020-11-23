0652b25db0b98458f0237f12c7e2080f79b8f78c
Daniel Bigos <daniel.bigos@icloud.com>
2020-11-23 04:35:52 +0100

    feat: start ABCI server

--- src/main/scala/com/bidzyyys/signchain/ABCI.scala
+++ src/main/scala/com/bidzyyys/signchain/ABCI.scala
@@ -1,51 +1,118 @@
 package com.bidzyyys.signchain
 
-import tendermint.abci.types.types.ABCIApplicationGrpc;
-import tendermint.abci.types.types.{RequestCommit, ResponseCommit}
-import tendermint.abci.types.types.{RequestFlush, ResponseFlush}
-import tendermint.abci.types.types.{RequestInfo, ResponseInfo}
-import tendermint.abci.types.types.{RequestEndBlock, ResponseEndBlock}
-import tendermint.abci.types.types.{RequestInitChain, ResponseInitChain}
-import tendermint.abci.types.types.{RequestSetOption, ResponseSetOption}
-import tendermint.abci.types.types.{RequestCheckTx, ResponseCheckTx}
-import tendermint.abci.types.types.{RequestEcho, ResponseEcho}
-import tendermint.abci.types.types.{RequestDeliverTx, ResponseDeliverTx}
-import tendermint.abci.types.types.{RequestBeginBlock, ResponseBeginBlock}
-import tendermint.abci.types.types.{RequestQuery, ResponseQuery}
+import java.util.logging.Logger
 
-import scala.concurrent.Future
+import io.grpc.{Server, ServerBuilder}
 
-object ABCI extends ABCIApplicationGrpc.ABCIApplication {
+import scala.concurrent.{ExecutionContext, Future}
 
-  override def echo(request: RequestEcho): Future[ResponseEcho] = ???
+import tendermint.abci.types.types.{
+  ABCIApplicationGrpc,
+  RequestCommit,
+  ResponseCommit,
+  RequestFlush,
+  ResponseFlush,
+  RequestInfo,
+  ResponseInfo,
+  RequestEndBlock,
+  ResponseEndBlock,
+  RequestInitChain,
+  ResponseInitChain,
+  RequestSetOption,
+  ResponseSetOption,
+  RequestCheckTx,
+  ResponseCheckTx,
+  RequestEcho,
+  ResponseEcho,
+  RequestDeliverTx,
+  ResponseDeliverTx,
+  RequestBeginBlock,
+  ResponseBeginBlock,
+  RequestQuery,
+  ResponseQuery
+}
 
-  override def flush(request: RequestFlush): Future[ResponseFlush] = ???
+object ABCIServer {
+  private val logger = Logger.getLogger(classOf[ABCIServer].getName)
 
-  override def info(request: RequestInfo): Future[ResponseInfo] = ???
+  def main(args: Array[String]): Unit = {
+    val server = new ABCIServer(ExecutionContext.global)
+    server.start()
+    server.blockUntilShutdown()
+  }
 
-  override def setOption(request: RequestSetOption): Future[ResponseSetOption] =
-    ???
+  private val port = 26658
+}
 
-  override def deliverTx(request: RequestDeliverTx): Future[ResponseDeliverTx] =
-    ???
+class ABCIServer(executionContext: ExecutionContext) { self =>
+  private[this] var server: Server = null
 
-  override def checkTx(request: RequestCheckTx): Future[ResponseCheckTx] = ???
+  private def start(): Unit = {
+    server = ServerBuilder
+      .forPort(ABCIServer.port)
+      .addService(
+        ABCIApplicationGrpc.bindService(new ABCIImpl, executionContext)
+      )
+      .build
+      .start
+    ABCIServer.logger.info(
+      "ABCI started, listening on " + ABCIServer.port
+    )
+    sys.addShutdownHook {
+      System.err.println(
+        "*** shutting down gRPC server since JVM is shutting down"
+      )
+      self.stop()
+      System.err.println("*** server shut down")
+    }
+  }
 
-  override def query(request: RequestQuery): Future[ResponseQuery] = ???
+  private def stop(): Unit = {
+    if (server != null) {
+      server.shutdown()
+    }
+  }
 
-  override def commit(request: RequestCommit): Future[ResponseCommit] = ???
+  private def blockUntilShutdown(): Unit = {
+    if (server != null) {
+      server.awaitTermination()
+    }
+  }
 
-  override def initChain(request: RequestInitChain): Future[ResponseInitChain] =
-    ???
+  private class ABCIImpl extends ABCIApplicationGrpc.ABCIApplication {
 
-  override def beginBlock(
-      request: RequestBeginBlock
-  ): Future[ResponseBeginBlock] = ???
+    override def echo(request: RequestEcho): Future[ResponseEcho] = ???
 
-  override def endBlock(request: RequestEndBlock): Future[ResponseEndBlock] =
-    ???
+    override def flush(request: RequestFlush): Future[ResponseFlush] = ???
 
-  def main(args: Array[String]) = {
-    println("Hello, world")
+    override def info(request: RequestInfo): Future[ResponseInfo] = ???
+
+    override def setOption(
+        request: RequestSetOption
+    ): Future[ResponseSetOption] =
+      ???
+
+    override def deliverTx(
+        request: RequestDeliverTx
+    ): Future[ResponseDeliverTx] =
+      ???
+
+    override def checkTx(request: RequestCheckTx): Future[ResponseCheckTx] = ???
+
+    override def query(request: RequestQuery): Future[ResponseQuery] = ???
+
+    override def commit(request: RequestCommit): Future[ResponseCommit] = ???
+
+    override def initChain(
+        request: RequestInitChain
+    ): Future[ResponseInitChain] =
+      ???
+
+    override def beginBlock(
+        request: RequestBeginBlock
+    ): Future[ResponseBeginBlock] = ???
+
+    override def endBlock(request: RequestEndBlock): Future[ResponseEndBlock] =
+      ???
   }
 }
